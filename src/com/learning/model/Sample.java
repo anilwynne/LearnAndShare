51e6c4128583adcb09479615fc05b713a11b4b72
Anil Wynne <anilwynne@hotmail.com>
2022-02-07 13:15:47 +0530

    adding the file

---
+++ src/com/learning/model/Sample.java
@@ -0,0 +1,12 @@
+package com.learning.model;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Word");
+		
+		System.out.println("This is Java");
+	}
+
+}
